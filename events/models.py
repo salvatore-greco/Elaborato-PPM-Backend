548771afe1877c08f941726ec87c642f21de0a4b
salvatore-greco <greco.salvatore03@gmail.com>
2025-06-18 19:13:33 +0200

    FEAT: made models for user with custom user class, event and registration

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from users.models import CustomUser
+
+
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=50)
+    place_name = models.CharField(max_length=50)
+    place_address = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+
+class Registration(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    is_attendee = models.BooleanField(default=True)
+    is_organizer = models.BooleanField(default=False)
