bd76a491c4dc52563b55d8bcfb7dbc1989963659
salvatore-greco <greco.salvatore03@gmail.com>
2025-06-21 14:58:28 +0200

    FEAT: made custom user manager, added custom user to settings

--- events/models.py
+++ events/models.py
@@ -8,7 +8,7 @@ class Events(models.Model):
     name = models.CharField(max_length=50)
     place_name = models.CharField(max_length=50)
     place_address = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=5) # Assuming that max digits consider also the digit before decimal
     date = models.DateTimeField(auto_now=True)
 
 class Registration(models.Model):
--- users/models.py
+++ users/models.py
@@ -1,7 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_args):
+        if email is None:
+            raise ValueError('Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_args)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
-    is_attendee = models.BooleanField(default=True)
-    is_organizer = models.BooleanField(default=False)
+    is_organizer = models.BooleanField(default=False) # Default is attendee
