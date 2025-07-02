5dc510baba8efdeaab7485200bf5aa7f261c413b
salvatore-greco <greco.salvatore03@gmail.com>
2025-07-02 18:27:41 +0200

    FIX: Properly handled time and date with proper widget on event form (for create and manage)

--- events/forms.py
+++ events/forms.py
@@ -8,6 +8,13 @@ from django.db import models
 
 
 class EventForm(ModelForm):
+    date = forms.SplitDateTimeField(
+        widget=forms.SplitDateTimeWidget(
+            date_attrs={'type': 'date'},
+            time_attrs={'type': 'time'},
+            time_format="%H:%M"
+        )
+    )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -17,3 +24,10 @@ class EventForm(ModelForm):
     class Meta:
         model = Events
         fields = ['name', 'place_name', 'place_address', 'price', 'date']
+        widgets = {
+            'date': forms.SplitDateTimeWidget(
+                date_attrs={'type': 'date'},
+                time_attrs={'type': 'time'},
+                time_format="%H:%M"
+            )
+        }
