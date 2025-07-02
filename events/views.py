6012ddc7f26bb391cfaf2cb21a0e40e75062fb28
salvatore-greco <greco.salvatore03@gmail.com>
2025-07-02 23:48:18 +0200

    feat: cannot update, delete, register to an event if is in the past

--- events/views.py
+++ events/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
+from django.db.models import F, ExpressionWrapper, BooleanField
+from django.db.models.functions import Now
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, UpdateView, CreateView
 from django.views.generic.edit import DeletionMixin
-
+from django.utils.timezone import now
 from events.forms import EventForm
 from events.models import Events
+from datetime import datetime, timezone
 
 # Create your views here.
 # def homepage_view(request):
@@ -17,20 +20,31 @@ from events.models import Events
 class HomepageView(ListView):
     model = Events
     template_name = 'home.html'
+    ordering = 'date'
 
-
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        today = now()
+        for e in queryset:
+            print(f'today {today} e.date {e.date}')
+            e.disabled = e.date < today
+        return queryset
 def event_details_view(request, id:int) -> HttpResponse :
     user = request.user
     event = get_object_or_404(Events, id=id)
+    event.disabled = event.date<now()
     registered = False
     display_toast = False
+    attendees = None
     attendance = event.registration.count()
     if request.method == 'GET':
         for ev_user in event.registration.all():
             if ev_user.id == user.id:
                 registered = True
+        if request.user.is_organizer and request.user.id == event.organizer_id_id:
+            attendees = event.registration.all()
     else:
-        if user.is_authenticated:
+        if user.is_authenticated and not event.disabled:
             data = int(request.POST.get('btn'))
             display_toast = True
             if data == 1:
@@ -39,7 +53,14 @@ def event_details_view(request, id:int) -> HttpResponse :
             elif data == 0:
                 event.registration.remove(user)
                 registered = False
-    return render(request, 'event-details.html', context={'event': event, 'registered': registered, 'toast': display_toast, 'attendance': attendance})
+    context = {
+        'event': event,
+        'registered': registered,
+        'toast': display_toast,
+        'attendance': attendance,
+        'attendees': attendees
+    }
+    return render(request, 'event-details.html', context=context)
 
 
 class ManageEventView(PermissionRequiredMixin, UpdateView, DeletionMixin):
@@ -48,6 +69,7 @@ class ManageEventView(PermissionRequiredMixin, UpdateView, DeletionMixin):
     permission_required = ('events.change_events', 'events.delete_events')
     raise_exception = True
     model = Events
+    extra_context = {'disabled':None}
 
     # Called only by delete from deletion mixin
     def get_success_url(self):
@@ -64,19 +86,19 @@ class ManageEventView(PermissionRequiredMixin, UpdateView, DeletionMixin):
         if request.user.id != event.organizer_id_id:
             raise PermissionDenied
         data = self.request.POST.get('btn')
-        if data == 'update':
+        if data == 'update' and not self.extra_context['disabled']:
             return super().post(request, *args, *kwargs)
-        elif data == 'delete':
+        elif data == 'delete' and not self.extra_context['disabled']:
             messages.add_message(self.request, messages.SUCCESS, 'delete')
             return super().delete(request, *args, *kwargs)
         return None
 
     def get(self, request, *args, **kwargs):
         event = self.get_object()
+        self.extra_context['disabled'] = event.date<now()
         if request.user.id != event.organizer_id_id:
             raise PermissionDenied
         return super().get(request, *args, **kwargs)
-
 
 class CreateEventView(PermissionRequiredMixin, CreateView):
     model = Events
