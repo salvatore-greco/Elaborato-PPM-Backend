addab8458b52ad94f0505c71dcb16b8c6833a2d0
salvatore-greco <greco.salvatore03@gmail.com>
2025-07-07 00:07:07 +0200

    INFO: using postgresql

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from django.urls import reverse
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +139,6 @@ LOGIN_URL='/users/login'
 MESSAGE_STORAGE='django.contrib.messages.storage.session.SessionStorage'
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+# DB stuff
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
